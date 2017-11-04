68375d947cefa26f897f22130d4e36eda12c02fc
NanoGurus12920 <33012916+NanoGurus12920@users.noreply.github.com>
2017-11-04 11:37:47 -0700

    Add files via upload

---
+++ Pigments.java
@@ -0,0 +1,117 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/*
+ *
+ * This is an example LinearOpMode that shows how to use
+ * a Modern Robotics Color Sensor.
+ *
+ * The op mode assumes that the color sensor
+ * is configured with a name of "sensor_color".
+ *
+ * You can use the X button on gamepad1 to toggle the LED on and off.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+@Autonomous(name = "Sensor: MR Color", group = "Sensor")
+//@Disabled
+public class Pigments extends LinearOpMode {
+
+  com.qualcomm.robotcore.hardware.ColorSensor pigments;    // Hardware Device Object
+
+
+  @Override
+  public void runOpMode() {
+
+
+    float hsvValues[] = {0F,0F,0F};
+    final float values[] = hsvValues;
+    int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+    final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+    boolean bPrevState = false;
+    boolean bCurrState = false;
+    boolean bLedOn = true;
+    pigments = hardwareMap.get(com.qualcomm.robotcore.hardware.ColorSensor.class, "sensor_color");
+    pigments.enableLed(bLedOn);
+
+    // wait for the start button to be pressed.
+    waitForStart();
+
+    // while the op mode is active, loop and read the RGB data.
+    // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+    while (opModeIsActive()) {
+
+      // check the status of the x button on either gamepad.
+
+      // check for button state transitions.
+
+
+        // button is transitioning to a pressed state. So Toggle LED
+        bLedOn = !bLedOn;
+        pigments.enableLed(bLedOn);
+      Color.RGBToHSV(pigments.red() * 8, pigments.green() * 8, pigments.blue() * 8, hsvValues);
+      telemetry.addData("LED", bLedOn ? "On" : "Off");
+      telemetry.addData("Clear", pigments.alpha());
+      telemetry.addData("Red  ", pigments.red());
+      telemetry.addData("Green", pigments.green());
+      telemetry.addData("Blue ", pigments.blue());
+      telemetry.addData("Hue", hsvValues[0]);
+
+      // change the background color to match the color detected by the RGB sensor.
+      // pass a reference to the hue, saturation, and value array as an argument
+      // to the HSVToColor method.
+      relativeLayout.post(new Runnable() {
+        public void run() {
+          relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+        }
+      });
+
+      telemetry.update();
+    }
+
+    // Set the panel back to the default color
+    relativeLayout.post(new Runnable() {
+      public void run() {
+        relativeLayout.setBackgroundColor(Color.WHITE);
+      }
+    });
+  }
+}
